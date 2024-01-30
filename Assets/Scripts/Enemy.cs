e63a114ff5165d76b46db54d2a75d9c279521ab0
Diego Carnavalli Bolsoni <dibolsoni@gmail.com>
2024-01-30 13:16:44 -0300

    enemy ai
    - enemy movement
    - enemy waypoints
    - ground plane

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float speed = 10f;
+    private Transform target;
+    private int waypointIndex = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        target = Waypoints.points[waypointIndex];
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
 
+    private void GetNextWaypoint()
+    {
+        if ((waypointIndex + 1)  >= Waypoints.points.Length) {
+            return;
+        }
+        waypointIndex++;
+        target = Waypoints.points[waypointIndex];
     }
 }
